1011dcc309852de64989c6b21f11cbd3d3dbbf3c
Roman Misyurin <roman.misyurin@percona.com>
2019-12-03 13:32:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Collapse } from "antd";
+import "antd/dist/antd.css"; // Import Ant Design styles by less entry
+const { Panel } = Collapse;
+
+function callback(key) {
+  console.log(key);
+}
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Collapse defaultActiveKey={["1"]} onChange={callback}>
+        <Panel header="This is panel header 1" key="1">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 2" key="2">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 3" key="3" disabled>
+          <p>{text}</p>
+        </Panel>
+      </Collapse>
     </div>
   );
 }
